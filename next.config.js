b31bf2ecb598a103b1766e730b7bb896d3ef005e
Ruban Manoj <rubanpmanoj@gmail.com>
2025-10-09 18:59:05 +0400

    second commit: Al Maghseel website

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 // next.config.js
-const repo = 'al-maghseel-website'; // 👈 if you change the repo name, update this
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
   output: 'export',
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
-  // If you add next/image later:
+  // If you add next/image later, also add:
   // images: { unoptimized: true },
 };
